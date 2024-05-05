8b8da40fcb3c3c3c5be4843804de844fadc56c84
Lucia <122565245+ll5zh@users.noreply.github.com>
2024-05-05 17:05:21 -0400

    try new redirect uri, remove 404

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,5 +1,9 @@
 export const CLIENT_ID = '7576b11b3736486ebc54dff50859fd0d';
-export const REDIRECT_URI = 'http://localhost:3000/spotify-trivia/redirect';
+
+// use appropriate redirect uri for deployed vs local setup
+export const REDIRECT_URI = 'https://spotify-trivia-sigma.vercel.app/spotify-trivia/redirect';
+// export const REDIRECT_URI = 'http://localhost:3000/spotify-trivia/redirect';
+
 export const SCOPE = 'user-read-private user-read-email user-top-read user-read-recently-played';
 export const BASE_URL = 'https://api.spotify.com/v1';
 
